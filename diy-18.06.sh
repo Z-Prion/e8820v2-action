a983068134d3522b7a0e3e2100919a7da6aae447
Z-Prion <63301611+Z-Prion@users.noreply.github.com>
2021-12-23 09:37:00 +0800

    Update diy-18.06.sh

--- diy-18.06.sh
+++ diy-18.06.sh
@@ -2,3 +2,4 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kiddin9_packages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
