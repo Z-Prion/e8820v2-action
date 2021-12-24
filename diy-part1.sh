84514a0e0985bf5be442dea0db12b74f88454527
Z-Prion <63301611+Z-Prion@users.noreply.github.com>
2021-12-24 18:43:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,8 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git 281677160 https://github.com/281677160/openwrt-package' >>feeds.conf.default
+# echo 'src-git 281677160 https://github.com/281677160/openwrt-package' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git kiddin9_packages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
